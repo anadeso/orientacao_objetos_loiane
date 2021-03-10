e0a1af679e3fa3bfaa2d9b5f46d6e8d284f29a6f
anadeso <anadeso90@gmail.com>
2021-03-10 08:15:41 -0300

    inicializacao exercicio

---
+++ src/aula_52/Agenda.java
@@ -0,0 +1,12 @@
+package aula_52;
+
+public class Agenda {
+
+    private Contato[] contatos;
+
+    private Agenda(){
+        contatos = new Contato[5];
+    }
+
+
+}
---
+++ src/aula_52/Teste.java
@@ -0,0 +1,17 @@
+package aula_52;
+
+public class Teste {
+
+    public static void main(String[] args) {
+
+        Contato c1 = new Contato();
+        System.out.println(c1);
+
+        Contato c2 = new Contato();
+        System.out.println(c2);
+
+        Contato c3 = new Contato();
+        System.out.println(c3);
+
+    }
+}
